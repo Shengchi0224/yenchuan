002dd0b6c8c9e1a0cd6b9d869b3f243c76bd4ada
Shengchi0224 <128444673+Shengchi0224@users.noreply.github.com>
2023-05-30 17:59:18 +0800

    Update text-anim.js

--- text-anim.js
+++ text-anim.js
@@ -1,75 +1,92 @@
- // Your animation code here
-        var texts = ["Enjoynable", "Memorable"];
-        var morphTime = 1;
-        var cooldownTime = 0.25;
-        var textIndex = texts.length - 1;
-        var time = new Date();
-        var morph = 0;
-        var cooldown = cooldownTime;
+function reloadAnimation() {
+ const elts = {
+	text1: document.getElementById("text1"),
+	text2: document.getElementById("text2")
+};
 
-        var elts = {
-          text1: document.getElementById("text1"),
-          text2: document.getElementById("text2")
-        };
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+	"Enjoynable",
+	"Memorable"
+];
 
-        function doMorph() {
-          morph -= cooldown;
-          cooldown = 0;
+// Controls the speed of morphing.
+const morphTime = 1;
+const cooldownTime = 0.25;
 
-          var fraction = morph / morphTime;
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
 
-          if (fraction > 1) {
-            cooldown = cooldownTime;
-            fraction = 1;
-          }
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
-          setMorph(fraction);
-        }
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
+	
+	setMorph(fraction);
+}
 
-        function setMorph(fraction) {
-     function setMorph(fraction) {
-      elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-      elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-      
-      fraction = 1 - fraction;
-      elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-      elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-      
-      elts.text1.textContent = texts[textIndex % texts.length];
-      elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-    }
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+	
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
 
-        function doCooldown() {
-          morph = 0;
+function doCooldown() {
+	morph = 0;
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
 
-          elts.text2.style.filter = "";
-          elts.text2.style.opacity = "100%";
+// Animation loop, which is called every frame.
+function animate() {
+	requestAnimationFrame(animate);
+	
+	let newTime = new Date();
+	let shouldIncrementIndex = cooldown > 0;
+	let dt = (newTime - time) / 1500;
+	time = newTime;
+	
+	cooldown -= dt;
+	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+}
 
-          elts.text1.style.filter = "";
-          elts.text1.style.opacity = "0%";
-        }
+// Start the animation.
+animate();
 
-        function animate() {
-          requestAnimationFrame(animate);
 
-          var newTime = new Date();
-          var shouldIncrementIndex = cooldown > 0;
-          var dt = (newTime - time) / 1500;
-          time = newTime;
-
-          cooldown -= dt;
-
-          if (cooldown <= 0) {
-            if (shouldIncrementIndex) {
-              textIndex++;
-            }
-
-            doMorph();
-          } else {
-            doCooldown();
-          }
-        }
-      if (window.location.pathname === '/') {
-        animate();
-        }
-
+    document.addEventListener("DOMContentLoaded", function(event) {
+      reloadgui();
+    }); 
