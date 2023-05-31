03ba27eccf0f25ca8b712862a7ed521de0849663
Shengchi0224 <128444673+Shengchi0224@users.noreply.github.com>
2023-05-31 17:05:36 +0800

    Update barba-init.js

--- barba-init.js
+++ barba-init.js
@@ -6,6 +6,7 @@ function delay(time) {
     }, time);
   });
 }
+
 function leaveAnimation() {
   console.log("leave");
   const tl = gsap.timeline();
@@ -13,43 +14,50 @@ function leaveAnimation() {
     duration: 1,
     height: "100vh",
     ease: "Power2.out",
-    marginTop:"0vh",
+    marginTop: "0vh",
   });
 }
 
 function enterAnimation() {
   const tl = gsap.timeline();
-  tl.fromTo(".loading-screen",{
-    height: "100vh",
-    marginTop:"0vh",
-  }, 
-  {
-    borderRadius: '40px',
-    duration: 1,
-    height:"30vh",
-    marginTop:"70vh",
-    ease: "Power2.out",
-    backgroundColor:"white",
-    onComplete: () => {
-    opacity:0,
-            },
-  });
+  tl.fromTo(
+    ".loading-screen",
+    {
+      height: "100vh",
+      marginTop: "0vh",
+    },
+    {
+      borderRadius: '40px',
+      duration: 1,
+      height: "30vh",
+      marginTop: "70vh",
+      ease: "Power2.out",
+      backgroundColor: "white",
+      onComplete: () => {
+        tl.to(".loading-screen", {
+          opacity: 0,
+        });
+      },
+    }
+  );
 }
 
 barba.init({
-  transitions: [{
-    name: 'opacity-transition',
-    async leave(data) {
-          const done = this.async();
-          leaveAnimation();
-          await delay(1000);
-          done();
+  transitions: [
+    {
+      name: 'opacity-transition',
+      async leave(data) {
+        const done = this.async();
+        leaveAnimation();
+        await delay(1000);
+        done();
+      },
+      async enter(data) {
+        const done = this.async();
+        enterAnimation();
+        await delay(1000);
+        done();
+      },
     },
-    async enter(data) {
-          const done = this.async();
-          enterAnimation();
-          await delay(1000);
-          done();
-    },
-  }]
+  ],
 });
