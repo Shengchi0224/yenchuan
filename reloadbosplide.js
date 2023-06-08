d5bd34b624971d5db95f32c81b0a43f2a330bdae
Shengchi0224 <128444673+Shengchi0224@users.noreply.github.com>
2023-06-08 12:21:07 +0800

    Update reloadbosplide.js

--- reloadbosplide.js
+++ reloadbosplide.js
@@ -6,6 +6,8 @@ function resetWebflow() {
   const dom = parser.parseFromString('<!doctype html><body>' + webflowPageId, 'text/html');
   webflowPageId = $(dom).find('body').text();
   $('html').attr('data-wf-page', webflowPageId);
+  console.log('Document reloaded');
+  
   window.Webflow && window.Webflow.destroy();
   window.Webflow && window.Webflow.ready();
   window.Webflow && window.Webflow.require('ix2').init();
