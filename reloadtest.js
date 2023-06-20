7ec0c265d8d64ca2c97f484860112e6f73c6329e
Shengchi0224 <128444673+Shengchi0224@users.noreply.github.com>
2023-06-20 13:20:03 +0800

    Update reloadtest.js

--- reloadtest.js
+++ reloadtest.js
@@ -115,7 +115,7 @@ function animateElements() {
     );
 }
 function replayVideos() {
-  const videos = document.querySelectorAll("#myVideo, #myVideo-1","#hero-bgvids","#hero-bgvids-1");
+  const videos = document.querySelectorAll("#myVideo, #myVideo-1, #hero-bgvids, #hero-bgvids-1");
   videos.forEach((video) => {
     video.currentTime = 0; // Reset the current time to the beginning
     video.play(); // Start playing the video
