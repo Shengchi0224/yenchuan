8467136f847511275d31852c90560ef42e97752f
Shengchi0224 <128444673+Shengchi0224@users.noreply.github.com>
2023-06-20 18:08:34 +0800

    Update reloadtest.js

--- reloadtest.js
+++ reloadtest.js
@@ -120,7 +120,7 @@ function reloadJS() {
             focus: 0,
             type: 'loop',
             gap: '2.66%',
-            padding: { left: '5%', right: '5%' } ,
+            padding:'5%',
             arrows: 'slider',
             pagination: 'slider',
             speed: 600,
